9e73b9d144850a9a267d89b8ea02ed9b1be76fba
Sai Prakash <coddinginjava@gmail.com>
2022-06-07 07:55:29 +0530

    inital commit mfe

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Mount from "./components/Marketing"
+
+export default () => {
+    return (
+        <div>
+            <h1>Contaier app</h1>
+            <hr />
+            <Mount />
+        </div>
+    );
+}
