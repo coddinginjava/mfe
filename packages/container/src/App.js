b07aa341b8b5b6cd4a7b43d9c7dad02c55341e47
Sai Prakash <coddinginjava@gmail.com>
2022-06-08 20:17:59 +0530

    dummy conmit after invalidation automation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import Mount from "./components/Marketing"
 export default () => {
     return (
         <div>
-            <h1>Contaier app</h1>
+            <h1>Contaier app !!</h1>
             <hr />
             <Mount />
         </div>
