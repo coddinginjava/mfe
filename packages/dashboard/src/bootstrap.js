37d399e974077e4166bb003e52a90bce2df3c700
Sai Prakash <coddinginjava@gmail.com>
2022-06-12 17:10:13 +0530

    dashboard yml corrected

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -7,8 +7,6 @@ const mount = (el) => {
   app.mount(el);
 };
 
-console.log("/dashbaoord")
-
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
